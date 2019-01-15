be7462930c6d82225907dd3f897286f54b60b2d4
androbianca <biaanca.9805@yahoo.com>
2019-01-15 16:43:43 +0200

    updates on kitty-profile

--- Pages/Profiles/Kitty/kitty-profile.js
+++ Pages/Profiles/Kitty/kitty-profile.js
@@ -1,96 +1,119 @@
 httpGet();
 
+function getId() {
+    var query = window.location.search.substring(1);
+    return query.split('id=')[1];
+}
+
+var baseUrl = '../Kitty/kitty-profile.html?id=';
 
 function httpGet() {
-    id= getId();
+    id = getId();
     const url = 'https://api.cryptokitties.co/kitties/' + id;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", url);
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            var response = JSON.parse(xmlHttp.responseText)
-            displayInfo(response);
+            var response = JSON.parse(xmlHttp.responseText);
+            var kitty = {
+                price: response.auction.current_price,
+                name: response.name,
+                profile_img: response.image_url_png,
+                description: response.bio,
+                cattributes: response.enhanced_cattributes,
+                children: response.children,
+                matron: response.matron,
+                sire: response.matron,
+                owner_name: response.owner.nickname,
+                owner_img: 'https://www.cryptokitties.co/profile/profile-' + response.owner.image + '.png',
+                owner_address: response.owner.address,
+            }
+            displayInfo(kitty);
         }
-
     }
     xmlHttp.send();
 }
 
-function getId(){
-  var query = window.location.search.substring(1);
-  return query.split('id=')[1];
-}
 
-function displayInfo(response) {
-   
-    displayCattributes(response.enhanced_cattributes);     
-    displayChildren(response.children);
-    url = 'https://www.cryptokitties.co/profile/profile-' + response.owner.image + '.png';
-    matron_url = '../Profiles/Kitty/kitty-profile.html?id=' + response.matron.id;
-    sire_url = '../Profiles/Kitty/kitty-profile.html?id=' + response.sire.id;
+function displayInfo(kitty) {
 
-    document.getElementById("profile-img").src = response.image_url_png;
-    document.getElementById("name").innerHTML = response.name;
-    document.getElementById('bio').innerHTML = response.bio;
-    document.getElementById("owner").innerHTML = response.owner.nickname;
-    document.getElementById('owner-img').src = url;
+    var parents = setParents(kitty);
+    var url = '../User/user-profile.html?id=' + kitty.owner_address;
 
-    if (!response.matron || !response.sire) {
-        document.getElementById("list").innerHTML = 'No parents found';
- 
-    } else {
-        document.getElementById("matron").src = response.matron.image_url_png;
-        var templateHtml = document.getElementById("matron").innerHTML;
-        templateHtml.replace(/{{href}}/g, matron_url);
-    
+    display(kitty.cattributes, 'displayCattributes', 'No cattributes', generateCattributes, "cattributes-template");
+    display(kitty.children, 'displayChildren', 'No children', generateFamily, 'family-template');
+    display(parents, 'displayparents', 'No parents found', generateFamily, 'family-template');
+
+    if (kitty.profile_img) {
+        document.getElementById("kitty-profile").src = kitty.profile_img;
+    } else
+        document.getElementById("kitty-profile").src = '../../unknown.png';
         
-        document.getElementById("sire").src = response.sire.image_url_png;      
-    }
+    document.getElementById("kitty-name").innerHTML = kitty.name;
+    document.getElementById('owner-profile').src = kitty.owner_img;
+    document.getElementById('owner-href').setAttribute('href', url);
+
+
+    setValue(kitty.description, 'description', 'No description');
+    setValue(kitty.owner_name, 'owner-name', 'Anonimus');
+    setValue(kitty.price, 'price', 'None yet');
 }
 
+function setParents(kitty) {
+    var parents = [];
 
-function displayCattributes(cattributes) {
-    if (generateChildren(cattributes)) {
-        document.getElementById("displayCattributes").innerHTML = generateCattributes(cattributes);
+    if (kitty.matron.id && kitty.sire.id) {
+        parents = [kitty.matron, kitty.sire];
+    }
+    if (!kitty.matron.id && kitty.sire.id) {
+        parents = [kitty.sire];
+    }
+    if (kitty.matron.id && !kitty.sire.id) {
+        parents = [kitty.matron];
+    }
+
+    return parents;
+}
+
+function setValue(item, id, string) {
+    if (item) {
+        document.getElementById(id).innerHTML = item;
         return;
     }
-    document.getElementById("displayCattributes").innerHTML = "No cattributes"
+    document.getElementById(id).innerHTML = string;
 }
 
 
-function displayChildren(children) {
-    if (generateChildren(children)) {
-        document.getElementById("displayChildren").innerHTML = generateChildren(children);
+function display(info, id, message, func, templateid) {
+    if (generateTemplate(info, templateid, func)) {
+        document.getElementById(id).innerHTML = generateTemplate(info, templateid, func);
         return;
     }
-    document.getElementById("displayChildren").innerHTML = "No children"
+    document.getElementById(id).innerHTML = message;
+}
+
+function generateFamily(list, element, templateHtml) {
+
+    var url = baseUrl + list[element].id;
+    return templateHtml.replace(/{{src}}/g, list[element]["image_url_png"])
+        .replace(/{{href}}/g, url)
 }
 
 
-function generateChildren(children) {
+function generateCattributes(list, element, templateHtml) {
+    return templateHtml.replace(/{{type}}/g, list[element]["type"])
+        .replace(/{{description}}/g, list[element]["description"])
+}
 
-    var template = document.getElementById("children-list");
+function generateTemplate(list, id, func) {
+
+    var template = document.getElementById(id);
     var templateHtml = template.innerHTML;
     var listHtml = "";
 
-    for (child in children) {
-        listHtml += templateHtml.replace(/{{href}}/g, children[child]["image_url_png"])
-    }
+    for (element in list) {
+        listHtml += func(list, element, templateHtml);
 
-    return listHtml;
-}
-
-function generateCattributes(cattributes) {
-
-
-    var template = document.getElementById("template-list-item");
-
-    var templateHtml = template.innerHTML;
-    var listHtml = "";
-
-    for (cattribute in cattributes) {
-        listHtml += templateHtml.replace(/{{type}}/g, cattributes[cattribute]["type"])
-            .replace(/{{description}}/g, cattributes[cattribute]["description"])
     }
 
     return listHtml;
